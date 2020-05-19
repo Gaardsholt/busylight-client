ed0ece52e9fd0ba944a15c5c53f74c496b71b007
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2020-05-19 21:15:14 +0200

    Initial work has been done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace busylight_client
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (OurIcon pi = new OurIcon())
+            {
+                pi.DisplayAsync();
+
+                Application.Run();
+            }
+        }
+    }
+}
